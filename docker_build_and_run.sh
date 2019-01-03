d6b9b26a0de8485fb37892112c8cac2f8fdc3439
Sebastian <sebastian.haderecker@gmail.com>
2019-01-03 21:51:35 +0100

    Create runnable Docker script
    - Builds a new Image from source
    - Removes old Containers
    - Runs a new Container

---
+++ docker_build_and_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build the Docker Image
+docker build -t twitter_bot .
+
+# Stop the perhaps already running Docker Container
+docker stop twitter_bot
+
+# Remove the old Docker Container
+docker container rm twitter_bot
+
+# Docker Run Command to start a new container and mount the directory
+docker run -d \
+    --name twitter_bot \
+    -v "/home/ubuntu/twitter_bot/:/twitter_bot/" \
+    --restart=unless-stopped \
+    twitter_bot
