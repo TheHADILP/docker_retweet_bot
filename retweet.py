8d344f16d37abbd6a327f08f1021cc2261a1c151
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2019-09-06 22:52:27 +0200

    Cleanup after pylint

--- retweet.py
+++ retweet.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python
 # coding: utf8
-
-import api_setup
+"""This script checks tweets of defined users for defined hashtags in random order.
+When a suitable tweet is found, which wasn't previously retweeted, it gets retweeted"""
 
 import random
 import time
 from twython import TwythonError
 
+import api_setup
+
 while True:
-    #checks if it contains certain hashtags + goes on until suitable tweet is found + checks if not previously retweeted by me
-    with open('/twitter_bot/users') as f:
-        users = f.read().splitlines()
+    with open('/twitter_bot/users') as user_list:
+        users = user_list.read().splitlines()
 
     while True:
         try:
             if len(users) > 0:
-                selected = users[random.randint(0,len(users))-1]
+                selected = users[random.randint(0, len(users))-1]
                 users.remove(selected)
                 print (selected)
 
                 try:
-                    timeline = api_setup.api.get_user_timeline(screen_name=selected, count=1, exclude_replies='true', include_rts='true')
+                    timeline = api_setup.api.get_user_timeline(
+                        screen_name=selected,
+                        count=1,
+                        exclude_replies='true',
+                        include_rts='true')
                 except TwythonError as e:
                     print (e)
 
                 for tweet in timeline:
                     nId = tweet['id_str']
 
-                    with open('/twitter_bot/buzzwords') as f:
-                        buzzwords = f.read().splitlines()
+                    with open('/twitter_bot/buzzwords') as buzzword_list:
+                        buzzwords = buzzword_list.read().splitlines()
 
                     if any(n in tweet['text'] for n in buzzwords):
 
@@ -40,16 +45,16 @@ while True:
                             with open('/twitter_bot/retweet-blacklist', 'a') as file:
                                 file.write('\n' + nId)
 
-                            api_setup.api.retweet(id = nId)
+                            api_setup.api.retweet(id=nId)
                             time.sleep(900)
                         else:
                             print ('     DUPLICATE FOUND')
                             time.sleep(2)
                             break
                     else:
-                         print ('     NOTHING FOUND YET')
-                         time.sleep(2)
-                         break
+                        print ('    No matching tweet found')
+                        time.sleep(2)
+                        break
             else:
                 print ('EMPTY, RESTARTING SCRIPT...')
                 time.sleep(5)
