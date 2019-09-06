cd765751b5a1228a2b6cbf932cc3c80dd644e8bb
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2019-09-06 21:36:25 +0200

    Cleanup after pylint

--- api_setup.py
+++ api_setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-import sys
-from twython import Twython, TwythonError
+"""This script reads the API credentials from a file
+and initializes Twython with them"""
+
+from twython import Twython
 
 with open('/twitter_bot/twitter-creds') as f:
     lines = f.read().splitlines()
